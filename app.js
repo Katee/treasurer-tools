f5340bb1ec9219d97cd3e25d39e9d3aff269bb39
katee <hello@kate.io>
2013-10-03 16:24:51 -0400

    Adds ability to send receipt email.

--- app.js
+++ app.js
@@ -18,20 +18,28 @@ var transport = nodemailer.createTransport("SMTP", {
 });
 
 var sendReminderEmail = function(email, userInformation) {
+  sendEmail(email, userInformation, 'Hacklab Dues Reminder', 'reminder');
+};
+
+var sendPaymentReceiptEmail = function(email, userInformation, paymentInformation) {
+  sendEmail(email, {user: userInformation, payment: paymentInformation}, 'Hacklab Dues Receipt', 'receipt');
+};
+
+var sendEmail = function(email, templateData, subject, templateName) {
   Q.nfcall(emailTemplates, options.email_templates_dir).then(function(template){
-    return Q.nfcall(template, 'reminder', userInformation);
+    return Q.nfcall(template, templateName, templateData);
   }).then(function(template){
     return Q.nfcall(transport.sendMail, {
       from: options.email_from,
       to: email,
-      subject: 'Hacklab Dues Reminder',
+      subject: subject,
       html: template[0],
       text: template[1]
     });
   }).then(function(responseStatus){
     console.log(responseStatus.message);
   }).then(function(){
-    console.log('Reminder email sent to %s', email);
+    console.log('%s sent to %s', subject, email);
   }, function(err){
     console.log(err);
   }).done();
