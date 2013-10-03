445854c157bff21fd7ac6cde0293f36656709630
katee <hello@kate.io>
2013-10-03 17:12:24 -0400

    Moves email stuff to its own module.

--- app.js
+++ app.js
@@ -1,46 +1,10 @@
 var path = require('path');
 var Q = require('q');
 var _ = require('underscore');
-var nodemailer = require("nodemailer");
-var emailTemplates = require('email-templates');
 
 // put your options in options.js to override these defaults
 var options = _.extend({
   email_templates_dir: path.join(__dirname, 'emails')
 }, require(path.join(path.dirname(module.filename), 'options')));
 
-var transport = nodemailer.createTransport("SMTP", {
-  service: "Gmail",
-  auth: {
-    user: options.email_username,
-    pass: options.email_password
-  }
-});
-
-var sendReminderEmail = function(email, userInformation, paymentsInformation) {
-  sendEmail(email, {user: userInformation, payments: paymentsInformation}, 'Hacklab Dues Reminder', 'reminder');
-};
-
-var sendPaymentReceiptEmail = function(email, userInformation, paymentInformation) {
-  sendEmail(email, {user: userInformation, payment: paymentInformation}, 'Hacklab Dues Receipt', 'receipt');
-};
-
-var sendEmail = function(email, templateData, subject, templateName) {
-  Q.nfcall(emailTemplates, options.email_templates_dir).then(function(template){
-    return Q.nfcall(template, templateName, templateData);
-  }).then(function(template){
-    return Q.nfcall(transport.sendMail, {
-      from: options.email_from,
-      to: email,
-      subject: subject,
-      html: template[0],
-      text: template[1]
-    });
-  }).then(function(responseStatus){
-    console.log(responseStatus.message);
-  }).then(function(){
-    console.log('%s sent to %s', subject, email);
-  }, function(err){
-    console.log(err);
-  }).done();
-};
+var emailer = require(path.join(path.dirname(module.filename), 'emailer'));
---
+++ emailer.js
@@ -0,0 +1,47 @@
+var Q = require('q');
+var nodemailer = require("nodemailer");
+var emailTemplates = require('email-templates');
+
+var sendReminderEmail = function(email, userInformation, paymentsInformation, options) {
+  sendEmail(email, {user: userInformation, payments: paymentsInformation}, 'Hacklab Dues Reminder', 'reminder', options);
+};
+
+var sendReceiptEmail = function(email, userInformation, paymentInformation, options) {
+  sendEmail(email, {user: userInformation, payment: paymentInformation}, 'Hacklab Dues Receipt', 'receipt', options);
+};
+
+module.exports.sendReminderEmail = sendReminderEmail;
+module.exports.sendReceiptEmail = sendReceiptEmail;
+
+function sendEmail(email, templateData, subject, templateName, options) {
+  var transport = makeTransport(options);
+
+  Q.nfcall(emailTemplates, options.email_templates_dir).then(function(template){
+    return Q.nfcall(template, templateName, templateData);
+  }).then(function(template){
+    return Q.nfcall(transport.sendMail, {
+      from: options.email_from,
+      to: email,
+      subject: subject,
+      html: template[0],
+      text: template[1]
+    });
+  }).then(function(responseStatus){
+    console.log(responseStatus.message);
+  }).then(function(){
+    console.log('%s sent to %s', subject, email);
+    transport.close();
+  }, function(err){
+    console.log(err);
+  }).done();
+}
+
+function makeTransport(options) {
+  return nodemailer.createTransport("SMTP", {
+    service: "Gmail",
+    auth: {
+      user: options.email_username,
+      pass: options.email_password
+    }
+  });
+}
